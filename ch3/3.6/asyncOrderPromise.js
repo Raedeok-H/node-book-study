29541afc8d7cdd4a5347ef281c1700ef966abf27
Raedeok-H <raedeokhwang@gmail.com>
2023-10-04 22:39:20 +0900

    asyncOrderPromise

--- ch3/3.6/asyncOrderPromise.js
+++ ch3/3.6/asyncOrderPromise.js
@@ -1,20 +1,30 @@
 const fs = require('fs').promises;
 
-console.log('시작');
-fs.readFile('./readme2.txt')
-  .then((data) => {
-    console.log('1번', data.toString());
-    return fs.readFile('./readme2.txt');
-  })
-  .then((data) => {
-    console.log('2번', data.toString());
-    return fs.readFile('./readme2.txt');
-  })
-  .then((data) => {
-    console.log('3번', data.toString());
-    console.log('끝');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+// console.log('시작');
+// fs.readFile('./readme2.txt')
+//   .then((data) => {
+//     console.log('1번', data.toString());
+//     return fs.readFile('./readme2.txt');
+//   })
+//   .then((data) => {
+//     console.log('2번', data.toString());
+//     return fs.readFile('./readme2.txt');
+//   })
+//   .then((data) => {
+//     console.log('3번', data.toString());
+//     console.log('끝');
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 
+// 아래는 더 나은 코드
+async function main() {
+  let data = await fs.readFile('./readme2.txt');
+  console.log('1번', data.toString());
+  data = await fs.readFile('./readme2.txt');
+  console.log('2번', data.toString());
+  data = await fs.readFile('./readme2.txt');
+  console.log('3번', data.toString());
+};
+main();
